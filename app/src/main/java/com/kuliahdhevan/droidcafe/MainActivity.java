cbf65f13eea8758f88351c3ac1db51a17c217883
Dhevan Muhamad <dhevanmuhamad@gmail.com>
2021-09-25 22:14:14 +0700

    done

---
+++ app/src/main/java/com/kuliahdhevan/droidcafe/MainActivity.java
@@ -0,0 +1,26 @@
+package com.kuliahdhevan.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMap(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:-6.982866,110.4069027");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+}
